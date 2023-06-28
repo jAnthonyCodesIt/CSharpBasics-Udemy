525254fffd9676cb69dc89cca1651f316b18d772
J. Anthony <janthonycodesit@outlook.com>
2023-06-28 10:47:39 -0400

    refactor: Extracted Text/String example to separate class

---
+++ TextExamples.cs
@@ -0,0 +1,90 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+
+class TextExamples
+{
+    public string? input { get; set; }
+
+    public void StringBuilderExample()
+    {
+        var builder = new StringBuilder("Hello World");
+        builder
+            .Append('-', 10)
+            .AppendLine()
+            .Append("Header")
+            .AppendLine()
+            .Append('-', 10)
+            .Replace("-", "+")
+            .Remove(0, 10)
+            .Insert(0, new string('-', 10));
+
+        Console.WriteLine(builder);
+
+        Console.WriteLine($"First Character: {builder[0]}");
+    }
+
+    public void Exercise1() 
+    {
+        /*
+        * 1- Write a program and ask the user to enter a few numbers separated by a hyphen. 
+        * Work out if the numbers are consecutive. 
+        * For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+        */
+        string inputNumbers1 = numberListPrompt();
+        var numberList = inputNumbers1.Split('-');
+        
+        //Check if numbers are consecutive
+        var consecutive = true;
+        for (int i = 1; i < numberList.Length; i++){
+            if (Convert.ToInt32(numberList[i]) < Convert.ToInt32(numberList[i - 1])){
+                consecutive = false;
+                break;
+            }
+        }
+        
+        if (string.IsNullOrEmpty(inputNumbers1))
+        {
+            Console.WriteLine("No input.");
+        }
+        else
+        {
+            Console.WriteLine($"The numbers {inputNumbers1} are . . . " + (consecutive ? "Consecutive" : "Not Consecutive"));
+        }
+
+    }
+
+    public void Exercise2()
+    {
+        /*
+        * 2- Write a program and ask the user to enter a few numbers separated by a hyphen.
+        * If the user simply presses Enter, without supplying an input, exit immediately;
+        * otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
+        */
+        string inputNumbers2 = numberListPrompt();
+        if (string.IsNullOrEmpty(inputNumbers2))
+        {
+            return;
+        }
+        else
+        {
+            //Check for duplicates
+            var numberList2 = inputNumbers2.Split('-');
+            for (int i = 0; i < numberList2.Length; i++)
+            {
+                var potentialDuplicates = numberList2.Where(s => s.Equals(numberList2[i]));
+                if (potentialDuplicates.Count() > 1) {
+                    Console.WriteLine("Duplicate");
+                    return;
+                }
+            }
+        }
+    }
+
+    public string numberListPrompt() {
+        //Get input from user
+        Console.WriteLine("Please enter a few numbers separated by a hyphen (ex. 5-2-7-4-9)");
+        input = Console.ReadLine() as string;
+        return input is not null ? input : "";
+    }
+}
